97bec3abd136cc4e7399a730ae8a563c52d65382
Kloudy <tim.kerr92@gmail.com>
2016-01-30 18:33:19 -0500

    initial commit

---
+++ SignGUI/src/com/antarescraft/kloudy/signguilib/SignGUIUpdateEvent.java
@@ -0,0 +1,39 @@
+package com.antarescraft.kloudy.signguilib;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class SignGUIUpdateEvent extends Event
+{
+	private static final HandlerList handlers = new HandlerList();
+	private String[] signText;
+	private Player player;
+	
+	public SignGUIUpdateEvent(Player player, String[] signText)
+	{
+		this.player = player;
+		this.signText = signText;
+	}
+	
+	@Override
+	public HandlerList getHandlers()
+	{
+	    return handlers;
+	}
+	
+	public static HandlerList getHandlerList()
+	{
+		return handlers;
+	}
+	
+	public Player getPlayer()
+	{
+		return player;
+	}
+	
+	public String[] getSignText()
+	{
+		return signText;
+	}
+}
